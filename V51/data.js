4a87ebbac751876d9074568a2ed580afb77a2a02
Shazlka <131820596+Shazlka@users.noreply.github.com>
2026-05-13 10:04:20 +0400

    تحديث data.js — 13‏/5‏/2026، 10:04:19 ص

--- V51/data.js
+++ V51/data.js
@@ -147,7 +147,8 @@ const DATA = [
     ],
     "note": "<b>الاعراف/العنكبوت</b>: الرجفة + دارهم <div><b>هود</b>: الصيحة + ديارهم</div><div><span><b>العنكبوت</b>: زيادة فكذبوه </span></div><div><span><b>هود٩٤</b>: تأنيث أخذتِ</span></div>",
     "unote": "",
-    "completed": true
+    "completed": true,
+    "locked": true
   },
   {
     "id": 2,
